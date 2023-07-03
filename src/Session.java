b9b061d7f9e81bd73b5f4802cffa828faa992dcb
ComfyChair <unknown.entity@gmx.de>
2023-07-03 14:46:00 +0200

    control flow outline for SessionManager and Session

--- src/Session.java
+++ src/Session.java
@@ -4,10 +4,28 @@ import java.util.Calendar;
 
 public class Session implements LuaToXML{
     private SessionInfo sessionInfo;
-    private ArrayList<Feature> featureList = new ArrayList<>();
+    private ArrayList<Feature> featureList;
 
     SessionInfo getSessionInfo(){
         return sessionInfo;
+    }
+
+    @Override
+    public boolean exportToXML(){
+        boolean canRead = readFeaturesFromLua();
+        boolean canWrite = writeXML();
+        return canRead && canWrite;
+    }
+
+    private boolean readFeaturesFromLua() {
+        featureList = new ArrayList<>();
+        //TODO: populate featureList from Lua file
+        return true;
+    }
+
+    private boolean writeXML() {
+        // TODO: write XML from features
+        return true;
     }
 
     record SessionInfo(int sessionID, String charname, String serverName, Calendar time){}
--- src/SessionManager.java
+++ src/SessionManager.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class SessionManager {
     private static SessionManager instance;
-    private static final ArrayList<Session.SessionInfo> sessionList = new ArrayList<>();
+    private static ArrayList<Session> sessionList = new ArrayList<>();
 
     private SessionManager(){}
 
@@ -15,15 +15,20 @@ public class SessionManager {
     }
 
     ArrayList<Session.SessionInfo> getSessionList(){
+        sessionList = sessionsFromLua();
+        ArrayList<Session.SessionInfo> sessionInfos = new ArrayList<>();
+        //TODO: compile Session info into ArrayList
+        return sessionInfos;
+    }
+
+    ArrayList<Session> sessionsFromLua(){
+        ArrayList<Session> sessionList = new ArrayList<>();
+        // TODO: populate session list by reading lua file and creating sessions
         return sessionList;
     }
 
-    void sessionFromLua(){
-
-    }
-
     void exportToXML(int sessionID){
-
+        sessionList.get(sessionID).exportToXML();
     }
 
 }
