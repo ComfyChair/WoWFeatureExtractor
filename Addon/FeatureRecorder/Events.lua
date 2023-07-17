29fe6510d91b277b7a7b53207994ff49d0842fb2
ComfyChair <unknown.entity@gmx.de>
2023-07-17 09:10:14 +0200

    integrated separate name/description table entries into FRT_EventEnum, removed testing parts from Events.lua

--- Addon/FeatureRecorder/Events.lua
+++ Addon/FeatureRecorder/Events.lua
@@ -3,8 +3,6 @@
 --- defines tracked events and links them to the type of interaction
 --- defines functions for retrieving additional information for each interaction type
 --- @author Jennifer Hanna
----
-local testing = false
 
 --- @table for functions to redirect to Blizzard API
 FRT_ObjectRetrievalCalls = {
@@ -56,42 +54,11 @@ FRT_ObjectRetrievalCalls = {
                     end
 }
 
---- replaces Blizzard API calls with mock functions for testing
-if testing then
-    FRT_ObjectRetrievalCalls.getZoneText = "Test Zone"
-    FRT_ObjectRetrievalCalls.getSubZoneText = "Test Subzone"
-    FRT_ObjectRetrievalCalls.getUnitName =  "Test Unit "
-    FRT_ObjectRetrievalCalls.getQuestName = "Test Quest"
-    FRT_ObjectRetrievalCalls.getTaxiMapName = "Test Map Name"
-    FRT_ObjectRetrievalCalls.getEventArgument = function() return "Test return from event argument"  end
-    FRT_ObjectRetrievalCalls.getLootItemNames = {"Test loot 1", "Test loot 2"}
-    FRT_ObjectRetrievalCalls.getGroupMembers = {"Hans", "Egon", "Berta"}
-end
-
---- @table of interaction types and their description
-local interactionType = {
-    MOV_1 = { name = "MOV_1", descr = "started moving"},
-    MOV_2 = {name = "MOV_2", descr = "stopped moving"},
-    ZONE_1 = {name = "ZONE_1", descr = "entered new area" },
-    ZONE_2 = {name = "ZONE_2", descr = "entered new subarea" },
-    ZONE_3 = {name = "ZONE_3", descr = "entered indoor area" },
-    OBJ_1 = {name = "OBJ_1", descr = "looting" },
-    OBJ_2 = {name = "OBJ_2", descr = "opened mailbox" },
-    NPC_1 = {name = "NPC_1", descr = "quest npc" },
-    NPC_2 = {name = "NPC_2", descr = "flight master" },
-    COMM_1 = {name = "COMM_1", descr = "emote" },
-    COMM_2 = {name = "COMM_2", descr = "whisper sent" },
-    COMM_3 = {name = "COMM_3", descr = "whisper received"},
-    COMM_4 = {name = "COMM_4", descr = "party chat" },
-    GRP_1 = {name = "GRP_1", descr = "grouping: group composition update" },
-    SPELL_1 = {name = "SPELL_1", descr = "sent spellcast" },
-    DUM_1 = {name = "DUM_1", descr = "dummy description" },
-}
---- @class
+--- @class FRT_Event
 FRT_Event = {   type = interactionType.DUM_1.name,
                 description = interactionType.DUM_1,
                 objectCalls = {}
-        }
+            }
 --- constructor
 function FRT_Event:new(iaType, descr, objectRetrievalCalls)
     local self = setmetatable({}, FRT_Event)
@@ -103,6 +70,7 @@ function FRT_Event:new(iaType, descr, objectRetrievalCalls)
 end
 
 FRT_Event.__index = FRT_Event
+--- @end class FRT_Event
 
 --- enum-like table of events with their type, description and  (optional) object retrieval functions
 --- can be extended by adding further events to the list
@@ -110,81 +78,80 @@ FRT_EventEnum = {
     --- Movements:
     -- objects are player character and zone, collected by Blizzard API functions
     ["PLAYER_STARTED_MOVING"] = FRT_Event:new(
-            interactionType.MOV_1.name,
-            interactionType.MOV_1.descr,
+            "MOV_1",
+            "started moving",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getZoneText}
             }) ,
     ["PLAYER_STOPPED_MOVING"] = FRT_Event:new(
-            interactionType.MOV_2.name,
-            interactionType.MOV_2.descr,
+            "MOV_2",
+            "stopped moving",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getZoneText}
             }),
+
     --- Zone change:
     -- objects are player character and zone, collected by Blizzard API functions
     ["ZONE_CHANGED_NEW_AREA"] = FRT_Event:new(
-            interactionType.ZONE_1.name,
-            interactionType.ZONE_1.descr,
+            "ZONE_1",
+            "entered new area" ,
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getZoneText}
             }),
     ["ZONE_CHANGED"] = FRT_Event:new(
-            interactionType.ZONE_2.name,
-            interactionType.ZONE_2.descr,
+            "ZONE_2",
+            "entered new subarea",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getSubZoneText}
             }),
     ["ZONE_CHANGED_INDOORS"] = FRT_Event:new(
-            interactionType.ZONE_3.name,
-            interactionType.ZONE_3.descr,
+                "ZONE_3",
+                "entered indoor area",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getSubZoneText}
             }),
 
     --- Object / NPC interactions:
-    -- objects are 1. object/npc name, 2. zone, 3. optional (loot items?, quest id)
     ["LOOT_OPENED"] = FRT_Event:new(
-            interactionType.OBJ_1.name,
-            interactionType.OBJ_1.descr, -- LOOT_READY fires twice, use LOOT_OPENED instead
+            "OBJ_1",
+            "looting", -- LOOT_READY fires twice, use LOOT_OPENED instead
             { { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getUnitName, "target"},
                 { FRT_ObjectRetrievalCalls.getZoneText},
                 {FRT_ObjectRetrievalCalls.getLootItemNames }
             }),
     ["MAIL_SHOW"] = FRT_Event:new(
-            interactionType.OBJ_2.name,
-            interactionType.OBJ_2.descr,
+            "OBJ_2",
+            "opened mailbox",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"},
                 { FRT_ObjectRetrievalCalls.getZoneText}
             }), -- mailbox: add mail headers/senders in the future?
     ["QUEST_DETAIL"] = FRT_Event:new(
-            interactionType.NPC_1.name,
-            interactionType.NPC_1.descr,
+            "NPC_1",
+            "quest npc",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "npc" },
                 { FRT_ObjectRetrievalCalls.getZoneText },
                 { FRT_ObjectRetrievalCalls.getQuestName }
             }),
     ["TAXIMAP_OPENED"] = FRT_Event:new(
-            interactionType.NPC_2.name,
-            interactionType.NPC_2.descr,
+            "NPC_2",
+            "flight master",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "npc" },
                 { FRT_ObjectRetrievalCalls.getTaxiMapName }
-            }), -- change to flight master id (maybe more significant)?
+            }),
 
     --- Player character interactions
-    -- most objects in this category get added upon feature creation
     ["CHAT_MSG_TEXT_EMOTE"] = FRT_Event:new(
-            interactionType.COMM_1.name,
-            interactionType.COMM_1.descr,
+            "COMM_1",
+            "emote",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "target" }, -- target = recipient
                 { FRT_ObjectRetrievalCalls.getEventArgument, 1 }, -- arg1: message
@@ -192,68 +159,48 @@ FRT_EventEnum = {
             }),
     -- whisper sent is no longer triggering CHAT_MSG_WHISPER, so we have to use a separate event
     ["CHAT_MSG_WHISPER_INFORM"] = FRT_Event:new(
-            interactionType.COMM_2.name,
-            interactionType.COMM_2.descr,
+            "COMM_2",
+            "whisper sent",
             {
                 { FRT_ObjectRetrievalCalls.getEventArgument, 1 }, -- arg1: message
                 { FRT_ObjectRetrievalCalls.getEventArgument, 2 }, -- arg2: recipient
             }),
     -- whisper received
     ["CHAT_MSG_WHISPER"] = FRT_Event:new(
-            interactionType.COMM_3.name,
-            interactionType.COMM_3.descr,
+            "COMM_3",
+            "whisper received",
             {
                 { FRT_ObjectRetrievalCalls.getEventArgument, 1 }, -- arg1: message
                 { FRT_ObjectRetrievalCalls.getEventArgument, 2 }, -- arg2: sender
             }),
     ["CHAT_MSG_PARTY"] = FRT_Event:new(
-            interactionType.COMM_4.name,
-            interactionType.COMM_4.descr,
+            "COMM_4",
+            "party chat",
             {
                 { FRT_ObjectRetrievalCalls.getEventArgument, 1 }, -- agr1: message
                 { FRT_ObjectRetrievalCalls.getEventArgument, 2 }, -- arg2: sender
             }),
     ["CHAT_MSG_PARTY_LEADER"] = FRT_Event:new(
-            interactionType.COMM_4.name,
-            interactionType.COMM_4.descr,
+            "COMM_4",
+            "party chat",
             {
                 { FRT_ObjectRetrievalCalls.getEventArgument, 1 }, -- agr1: message
                 { FRT_ObjectRetrievalCalls.getEventArgument, 2 }, -- arg2: sender
             }),
     ["GROUP_ROSTER_UPDATE"] = FRT_Event:new(
-            interactionType.GRP_1.name,
-            interactionType.GRP_1.descr,
+            "GRP_1",
+            "grouping: group composition update",
             {
                 {FRT_ObjectRetrievalCalls.getGroupMembers}
             } ),
-    -- spellcast: can have all sorts of targets
+
+    --- Spellcast: can have all sorts of targets
     ["UNIT_SPELLCAST_SENT"] = FRT_Event:new(
-            interactionType.SPELL_1.name,
-            interactionType.SPELL_1.descr,
+            "SPELL_1",
+            "sent spellcast",
             {
                 { FRT_ObjectRetrievalCalls.getUnitName, "player"}, -- sender is always the player
                 { FRT_ObjectRetrievalCalls.getEventArgument, 2 }, -- arg2: target of the spell
                 { FRT_ObjectRetrievalCalls.getEventArgument, 4 } -- arg5: spellID
             })
-
 }
-
--------------          TESTING AREA         ------------------------
------ Unit testing starts
-
-if testing then
-    lu = require('luaunit')
-    TestFRT_EventEnum = {} --class
-    function TestFRT_EventEnum:testQuestBasics()
-        local mockFeature = FRT_Event:new("QUEST_DETAIL", "NPC_1", "interacting with quest npc",{"Theo QuestTest", "Testalia Nord", 1234})
-        local expected = { eventName="QUEST_DETAIL", type="NPC_1", description="interacting with quest npc", objects={"Theo QuestTest", "Testalia Nord", "questID=1234"}}
-        lu.assertEquals(mockFeature, expected)
-    end
-    function TestFRT_EventEnum:testGetByNameQuest()
-        local mockFeature = FRT_EventEnum:getByName("QUEST_DETAIL")
-        local expected = { eventName="QUEST_DETAIL", type="NPC_1", description="interacting with quest npc", objects={"Test npc", "Test Zone", 1234}}
-        lu.assertEquals(mockFeature, expected)
-    end
-    -- class TestFRT_EventEnum
-    os.exit(lu.LuaUnit.run())
-end
