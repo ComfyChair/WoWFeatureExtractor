e7978f18ef457d00413cdde771282df799c4970b
ComfyChair <unknown.entity@gmx.de>
2023-07-21 18:00:12 +0200

    small button text changes

---
+++ src/test/resources/TwentySessions.lua
@@ -0,0 +1,292 @@
+
+FRT_FeatureRecordings = {
+	["session_9"] = {
+		["characterName"] = "Anton",
+		["serverName"] = "Antonidas",
+		["dateTime"] = 1679581448,
+		["featureTable"] = {
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_2",
+				["time"] = 1689923094,
+				["description"] = "stopped moving",
+			}, -- [1]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Öffnen", -- [2]
+				},
+				["type"] = "SPELL_1",
+				["time"] = 1689923095,
+				["description"] = "sent spellcast",
+			}, -- [2]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Flinky IV", -- [2]
+					"Das Auge Azsharas", -- [3]
+					"Kauknochen", -- [4]
+				},
+				["type"] = "OBJ_1",
+				["time"] = 1689923096,
+				["description"] = "looting",
+			}, -- [3]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_1",
+				["time"] = 1689923100,
+				["description"] = "started moving",
+			}, -- [4]
+		},
+	},
+	["session_1"] = {
+		["characterName"] = "Berta",
+		["serverName"] = "TestServer1",
+		["dateTime"] = 1676624245,
+		["featureTable"] = {
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_2",
+				["time"] = 1689922925,
+				["description"] = "stopped moving",
+			}, -- [1]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_1",
+				["time"] = 1689922929,
+				["description"] = "started moving",
+			}, -- [2]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_2",
+				["time"] = 1689922936,
+				["description"] = "stopped moving",
+			}, -- [3]
+		},
+	},
+	["session_5"] = {
+		["characterName"] = "Carlos",
+		["serverName"] = "TestServer2",
+		["dateTime"] = 1685624123,
+		["featureTable"] = {
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_2",
+				["time"] = 1689923028,
+				["description"] = "stopped moving",
+			}, -- [1]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_1",
+				["time"] = 1689923028,
+				["description"] = "started moving",
+			}, -- [2]
+		},
+	},
+	["session_12"] = {
+		["characterName"] = "Dora",
+		["serverName"] = "TestServer3",
+		["dateTime"] = 1689579731,
+		["featureTable"] = {
+					{
+        				["objects"] = {
+        					"Arvensis", -- [1]
+        					"Das Auge Azsharas", -- [2]
+        				},
+        				["type"] = "MOV_2",
+        				["time"] = 1689923100,
+        				["description"] = "stopped moving",
+        			}, -- [5]
+        			{
+        				["objects"] = {
+        					"Arvensis", -- [1]
+        					"Das Auge Azsharas", -- [2]
+        				},
+        				["type"] = "MOV_1",
+        				["time"] = 1689923103,
+        				["description"] = "started moving",
+        			}, -- [6]
+		},
+	},
+	["session_7"] = {
+		["characterName"] = "Esme",
+		["serverName"] = "TestServer3",
+		["dateTime"] = 1689580415,
+		["featureTable"] = {
+		},
+	},
+	["session_20"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923130,
+		["featureTable"] = {
+		},
+	},
+	["session_11"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923111,
+		["featureTable"] = {
+		},
+	},
+	["session_6"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923032,
+		["featureTable"] = {
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_2",
+				["time"] = 1689923035,
+				["description"] = "stopped moving",
+			}, -- [1]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_1",
+				["time"] = 1689923036,
+				["description"] = "started moving",
+			}, -- [2]
+		},
+	},
+	["session_16"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923123,
+		["featureTable"] = {
+		},
+	},
+	["session_18"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923127,
+		["featureTable"] = {
+		},
+	},
+	["session_8"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923066,
+		["featureTable"] = {
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_2",
+				["time"] = 1689923068,
+				["description"] = "stopped moving",
+			}, -- [1]
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_1",
+				["time"] = 1689923068,
+				["description"] = "started moving",
+			}, -- [2]
+		},
+	},
+	["session_3"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923013,
+		["featureTable"] = {
+		},
+	},
+	["session_13"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923116,
+		["featureTable"] = {
+		},
+	},
+	["session_19"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923128,
+		["featureTable"] = {
+		},
+	},
+	["session_17"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923125,
+		["featureTable"] = {
+		},
+	},
+	["session_15"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923120,
+		["featureTable"] = {
+		},
+	},
+	["session_14"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923118,
+		["featureTable"] = {
+		},
+	},
+	["session_2"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689922996,
+		["featureTable"] = {
+		},
+	},
+	["session_10"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923108,
+		["featureTable"] = {
+		},
+	},
+	["session_4"] = {
+		["characterName"] = "Foo",
+		["serverName"] = "Bar",
+		["dateTime"] = 1689923018,
+		["featureTable"] = {
+			{
+				["objects"] = {
+					"Arvensis", -- [1]
+					"Das Auge Azsharas", -- [2]
+				},
+				["type"] = "MOV_1",
+				["time"] = 1689923018,
+				["description"] = "started moving",
+			}, -- [1]
+		},
+	},
+}
+FRT_BtnData = {
+	["minimapPos"] = 243.2038631239735,
+}
